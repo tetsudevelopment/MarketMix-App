7285e8ee6f4a29bd142e2301850d805009055d3b
tetsudevelopment <bresmacawork@gmail.com>
2022-06-09 13:11:39 -0500

    Variables creadas

--- BackEnd/js/main.js
+++ BackEnd/js/main.js
@@ -24,14 +24,42 @@ var app = new Vue({
           pass: 1234,
         },
       ],
-      baseSalary:0,
+      dataAdmin: [
+        {
+          idAdmin: 1,
+          name: "Secretaria",
+          baseSalary: 1000000,
+        },
+        {
+          idAdmin: 2,
+          name: "Vendedor",
+          baseSalary: 2000000,
+          commission: 10,
+        },
+        {
+          idAdmin: 3,
+          name: "Ensamblador",
+          baseSalary: 3000000,
+          maxShoes: 0,
+          maxSneakers: 0,
+        },
+      ],
+      baseSalary: 0, //salario base
+      commission: 0, //vendedor-admin
+      maxShoes: 0, //ensamblador-admin
+      maxSneakers: 0, //ensamblador-admin
+      saleShoes: 0, //vendedor
+      saleSneakers: 0, //vendedor
+      joinShoes: 0, //ensamblador
+      joinSneakers: 0, //ensamblador
+      son: 0, //ensamblador
       extraHours: 0, //horas extras
       watchMain: 0, //vista principal
-      watchAdmin: 0,// vista del Admin
-      watchSons: 0,// Vista a pregunta tiene hijos?
+      watchAdmin: 0, // vista del Admin
+      watchSons: 0, // Vista a pregunta tiene hijos?
       pass: "1234", //contraseña
-      id: 0,
-      idAdmin: 0,
+      id: 0, //usuarios
+      idAdmin: 0, // usuarios de admin
     };
   },
 
@@ -66,6 +94,10 @@ var app = new Vue({
       // console.log(this.watch);
       console.log(data);
     },
+    //operaciones
+    
+
+    //botones
     admin: function () {
       const watchAdmin = parseInt(this.watchAdmin);
       if (watchAdmin === 1) return (this.watchAdmin = 1);
@@ -79,7 +111,36 @@ var app = new Vue({
       if (watchSons === 3) return (this.watchSons = 3);
     },
     save: function () {
-      this.watchAdmin = 0;
+      const id = parseInt(this.id)
+      if (id === 1) {
+        const idAdmin = parseInt(this.watchAdmin)
+        const dataAdmin = this.dataAdmin.forEach((element) => {
+          if (idAdmin === element.idAdmin) {
+            const baseSalary = element.baseSalary;
+            console.log(baseSalary);
+            return this.baseSalary= baseSalary
+          } 
+        })
+        
+        if (idAdmin === 1) {
+          alert("Secretaria");
+        } else if (idAdmin === 2) {
+          alert("Vendedor");
+        } else if (idAdmin === 3) {
+          alert("Ensamblador");
+        } else {
+          alert('Seleccione el cargo que desea modificar')
+        }
+      } else if (id === 2) {
+        alert("Secretaria");
+      } else if (id === 3) {
+        alert("Vendedor");
+      } else if (id === 4) {
+        alert("Ensamblador");
+      } else {
+        alert('Error')
+      }
+
     },
   },
 }); 
